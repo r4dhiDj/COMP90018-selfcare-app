45b2ddb580280428556414fc426696f6b91c32f3
patwdj <pwidjojo@student.unimelb.edu.au>
2021-10-27 22:22:03 +1100

    update login and register screen ui

--- app/src/main/java/com/example/selfcare/presentation/components/screens/LoginScreen.kt
+++ app/src/main/java/com/example/selfcare/presentation/components/screens/LoginScreen.kt
@@ -3,17 +3,29 @@ package com.example.selfcare.presentation.components.screens
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.ui.foundation.Image
+import androidx.ui.res.imageResource
+import androidx.ui.res.vectorResource
+import com.example.selfcare.R
 import com.example.selfcare.presentation.components.Screen
 import com.example.selfcare.viewmodels.MainViewModel
 import com.google.firebase.auth.ktx.auth
@@ -26,78 +38,84 @@ fun LoginScreen(viewModel: MainViewModel, navController: NavController, activity
     val auth = Firebase.auth
     var email by remember {mutableStateOf("")}
     var password by remember {mutableStateOf("")}
+    var passwordVisibility by remember {mutableStateOf(true)}
     if (Firebase.auth.currentUser!= null) {
         navController.popBackStack()
         navController.navigate(Screen.WelcomeScreen.route)
     }
-    Card(
+
+
+    Box(
         modifier = Modifier
             .fillMaxSize(),
-        elevation = 8.dp
+        contentAlignment = Alignment.BottomCenter
     ) {
-        Column {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp, bottom = 30.dp, start = 20.dp, end = 20.dp),
-                Arrangement.SpaceBetween,
-                Alignment.CenterVertically,
-            ) {
-                Text(text = "Login to your account", style = MaterialTheme.typography.h1)
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp, bottom = 12.dp, start = 20.dp, end = 20.dp),
-                Arrangement.SpaceBetween,
-                Alignment.CenterVertically,
-            ) {
+        Spacer(modifier = Modifier.padding(20.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            contentAlignment = Alignment.TopCenter
+        ){
+            androidx.compose.foundation.Image(
+                painter = painterResource(id = R.drawable.meditation2),
+                contentDescription = "login image",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Fit
+            )
+        }
+
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.55f)
+                .clip(RoundedCornerShape(30.dp))
+                .background(Color.White)
+                .padding(10.dp)
+        ) {
+            Text(
+                text = "Log In",
+                style = MaterialTheme.typography.h1
+            )
+            Spacer(modifier = Modifier.padding(20.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally){
                 OutlinedTextField(
                     value = email,
                     onValueChange = { email = it },
                     singleLine = true,
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(0.8f),
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Email
                     ),
                     label = { Text("Enter your email") })
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 12.dp, bottom = 12.dp, start = 20.dp, end = 20.dp),
-                Arrangement.SpaceBetween,
-                Alignment.CenterVertically
-            )
-            {
+
                 OutlinedTextField(
                     value = password,
                     onValueChange = { password = it },
                     singleLine = true,
-                    modifier = Modifier.fillMaxWidth(),
+                    trailingIcon = {
+                        IconButton(
+                            content = {
+                                Icon(
+                                    painter = painterResource(id = if(passwordVisibility) R.drawable.eye_on else R.drawable.eye_off),
+                                    contentDescription = "eye",
+                                    tint = if(passwordVisibility) Color.Black else Color.Gray)
+                            },
+                            onClick = { passwordVisibility = !passwordVisibility }
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth(0.8f),
                     label = { Text("Enter your password") },
-                    visualTransformation = PasswordVisualTransformation(),
+                    visualTransformation = if(passwordVisibility)
+                        VisualTransformation.None else PasswordVisualTransformation(),
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Password
                     )
                 )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 40.dp, bottom = 12.dp, start = 20.dp, end = 20.dp),
-                Arrangement.SpaceBetween,
-                Alignment.CenterVertically
-            )
-            {
-                Button(
-                    onClick = {
-                        navController.popBackStack()
-                        navController.navigate(Screen.RegisterScreen.route)
-                    }
-                ){
-                    Text(color = Color.White, text = "Sign-up instead")
-                }
+                Spacer(modifier = Modifier.padding(20.dp))
                 Button(
                     enabled = !(email.trim() == "" || password.trim() ==""),
                     onClick = {
@@ -113,13 +131,6 @@ fun LoginScreen(viewModel: MainViewModel, navController: NavController, activity
                                     while(Firebase.auth.currentUser== null){
                                         Log.d("waiting to login","")
                                     }
-//                                    val database = Firebase.database("https://kotlin-self-care-default-rtdb.firebaseio.com/").reference
-//                                    database.child("users").child(Firebase.auth.currentUser!!.uid).get().addOnSuccessListener {
-//                                        Log.i("firebase", "Got value ${it.value}")
-//                                    }.addOnFailureListener{
-//                                        Log.e("firebase", "Error getting data", it)
-//                                    }
-
                                     navController.navigate(Screen.WelcomeScreen.route)
                                 } else {
                                     Log.d("Login", "Failed: ${task.exception}")
@@ -133,7 +144,15 @@ fun LoginScreen(viewModel: MainViewModel, navController: NavController, activity
                 ) {
                     Text(color = Color.White, text = "Login")
                 }
-
+                Spacer(modifier = Modifier.padding(10.dp))
+                Text(
+                    text = "Sign Up instead",
+                    modifier = Modifier.clickable(onClick = {
+                        navController.popBackStack()
+                        navController.navigate(Screen.RegisterScreen.route)
+                    })
+                )
+                Spacer(modifier = Modifier.padding(20.dp))
             }
 
         }
