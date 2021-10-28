22f48dd565d04d5fc32602302379b07189eaa471
Thomas Zhao <yyzhao0526@gmail.com>
2021-10-28 19:33:39 +1100

    chat screen read example

--- app/src/main/java/com/example/selfcare/presentation/components/ChatScreen.kt
+++ app/src/main/java/com/example/selfcare/presentation/components/ChatScreen.kt
@@ -260,10 +260,15 @@ private fun addTextToChat(
     var date = dateTime.substring(0,10)
     date = date.replace("/", "-")
     val time = dateTime.substring(11,16)
-    Log.d("chatdate", date)
 
     val map = hashMapOf<String, String>()
     // change the following line
     map[time] = chat.toString()
     userRef.child("messages").child(date).setValue(map)
+    // read example
+    userRef.child("email").get().addOnSuccessListener {
+        Log.d("chat_database", "${it.value}")
+    }.addOnFailureListener{
+        Log.d("chat_database", "read error")
+    }
 }
